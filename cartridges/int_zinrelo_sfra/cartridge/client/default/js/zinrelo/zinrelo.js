7942860b33d6e0825d9bbb031c1ff062317f79be
rameshgh1 <108051267+rameshgh1@users.noreply.github.com>
2024-02-23 20:02:27 +0530

    feat: in cart reward redeem implementation

--- cartridges/int_zinrelo_sfra/cartridge/client/default/js/zinrelo/zinrelo.js
+++ cartridges/int_zinrelo_sfra/cartridge/client/default/js/zinrelo/zinrelo.js
@@ -17,6 +17,7 @@ function initZinreloDashboard() {
                         partnerID, jwtToken, version, server
                     } = response.userAuthData;
 
+                    // Initialize the dashbaord widget
                     window._zrl = window._zrl || [];
                     // eslint-disable-next-line no-undef
                     _zrl.push(['init', {
@@ -33,26 +34,93 @@ function initZinreloDashboard() {
 }
 
 /**
+ * Handles for rewards operations
+ * @param {Object} e current event
+ */
+function handleRewardAjax(e) {
+    e.preventDefault();
+    const url = $(this).attr('action');
+    const method = $(this).attr('method');
+    var data = $(this).serialize();
+
+    // Add csrf token
+    const $inCartRedemptionCsrfToken = $('.inCartRedemptionCsrfToken');
+    data += '&' + $inCartRedemptionCsrfToken.attr('name') + '=' + $inCartRedemptionCsrfToken.val();
+
+    if (!url) {
+        return;
+    }
+
+    $.spinner().start();
+    $.ajax({
+        url: url,
+        method: method,
+        data: data,
+        success: function (result) {
+            $.spinner().stop();
+            if (result.success) {
+                // Refresh the in-cart redemption section
+                $('body').trigger('renderInCartRedemptionSection');
+            }
+        },
+        error: function () {
+            $.spinner().stop();
+        }
+    });
+}
+
+/**
+ * Binds events to in-cart redemption section
+ */
+function bindEvents() {
+    var $redeemRewardForm = $('.redeemRewardForm');
+    var $cancleRewardForm = $('.cancleRewardForm');
+    var $zinreloRewardsDropdown = $('.zinreloRewardsDropdown');
+    var $redeemZinreloRewardBtn = $('.redeemZinreloRewardBtn');
+
+    if (!$zinreloRewardsDropdown.val()) {
+        $redeemZinreloRewardBtn.attr('disabled', true);
+    }
+
+    $zinreloRewardsDropdown.on('change', function () {
+        if ($(this).val()) {
+            $redeemZinreloRewardBtn.attr('disabled', false);
+        } else {
+            $redeemZinreloRewardBtn.attr('disabled', true);
+        }
+    });
+
+    $redeemRewardForm.on('submit', handleRewardAjax);
+    $cancleRewardForm.on('submit', handleRewardAjax);
+}
+
+/**
  * Renders in cart redemption section in checkout
  */
-function initInCartRedemption() {
-    var inCartRedemptionURL = $('.inCartRedemptionURL').val();
-    var $inCartRedemptionContainer = $('.inCartRedemptionContainer');
+function renderInCartRedemptionSection() {
+    $('body').on('renderInCartRedemptionSection', function () {
+        var inCartRedemptionURL = $('.inCartRedemptionURL').val();
+        var $inCartRedemptionContainer = $('.inCartRedemptionContainer');
 
-    if ($inCartRedemptionContainer.length > 0 && inCartRedemptionURL) {
-        $.spinner().start();
-        $.ajax({
-            url: inCartRedemptionURL,
-            method: 'GET',
-            success: function (result) {
-                $inCartRedemptionContainer.html(result);
-                $.spinner().stop();
-            },
-            error: function () {
-                $.spinner().stop();
-            }
-        });
-    }
+        if ($inCartRedemptionContainer.length > 0 && inCartRedemptionURL) {
+            $.spinner().start();
+            $.ajax({
+                url: inCartRedemptionURL,
+                method: 'GET',
+                success: function (result) {
+                    $inCartRedemptionContainer.html(result);
+                    bindEvents();
+                    $.spinner().stop();
+                },
+                error: function () {
+                    $.spinner().stop();
+                }
+            });
+        }
+    });
+
+    // Initial render
+    $('body').trigger('renderInCartRedemptionSection');
 }
 
 
@@ -60,6 +128,6 @@ module.exports = function (currentScript) {
     script = currentScript;
     return {
         initZinreloDashboard: initZinreloDashboard,
-        initInCartRedemption: initInCartRedemption
+        renderInCartRedemptionSection: renderInCartRedemptionSection
     };
 };
--- cartridges/int_zinrelo_sfra/cartridge/scripts/services/zinreloLoyaltyServices.js
+++ cartridges/int_zinrelo_sfra/cartridge/scripts/services/zinreloLoyaltyServices.js
@@ -2,25 +2,30 @@
 
 const LocalServiceRegistry = require('dw/svc/LocalServiceRegistry');
 
+const urlFormatter = require('*/cartridge/scripts/utils/urlFormatter');
 const { getAPIKey, getParnerID } = require('*/cartridge/scripts/helpers/zinreloPreferencesHelpers');
 const serviceName = 'int_zinrelo.http.loyalty.service';
 
 /**
  * Creates zinrelo loyalty service as per the provided endpoint and method
- * @param {string} endpoint zinrelo loyalty service endpoint
- * @param {string} method request method
+ * @param {Object} serviceOptions service options such as endpoint, request method etc
  * @return {dw.svc.Service} zirelo loyalty service
  */
-function createZinreloLoyaltyService(endpoint, method) {
+function createZinreloLoyaltyService(serviceOptions) {
     var zinreloLoyaltyService = LocalServiceRegistry.createService(serviceName, {
         createRequest: function (svc, params) {
-            svc.setURL(svc.URL + endpoint);
-            svc.setRequestMethod(method);
-            svc.addParam('idParam', 'member_id');
+            svc.setURL(svc.URL + serviceOptions.endpoint);
+            svc.setRequestMethod(serviceOptions.method);
+
+            if (serviceOptions.params) {
+                var finalURL = urlFormatter.addParamsToURL(svc.URL, serviceOptions.params);
+                svc.setURL(finalURL);
+            }
 
             // Add authorization headers
             svc.addHeader('api-key', getAPIKey());
             svc.addHeader('partner-id', getParnerID());
+            svc.addHeader('Content-Type', 'application/json');
 
             return JSON.stringify(params);
         },
