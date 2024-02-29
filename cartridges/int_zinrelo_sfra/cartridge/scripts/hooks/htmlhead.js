8ca6ca7263711a193306f394ace7fb42fbbb93d9
rameshgh1 <108051267+rameshgh1@users.noreply.github.com>
2024-02-29 12:21:28 +0530

    fix: added zinrelo enable flag to html head

--- cartridges/int_zinrelo_sfra/cartridge/scripts/hooks/htmlhead.js
+++ cartridges/int_zinrelo_sfra/cartridge/scripts/hooks/htmlhead.js
@@ -6,8 +6,11 @@ const ISML = require('dw/template/ISML');
  * Implements the html head hook to allow adding global javascript for zinrelo
  */
 function htmlHead() {
-    const URLUtils = require('dw/web/URLUtils');
-    ISML.renderTemplate('zinrelo/zinreloHeadScript', { authDataURL: URLUtils.url('Zinrelo-UserAuthData') });
+    const zinreloPreferencesHelpers = require('*/cartridge/scripts/helpers/zinreloPreferencesHelpers');
+    if (zinreloPreferencesHelpers.isZinreloEnabled()) {
+        const URLUtils = require('dw/web/URLUtils');
+        ISML.renderTemplate('zinrelo/zinreloHeadScript', { authDataURL: URLUtils.url('Zinrelo-UserAuthData') });
+    }
 }
 
 module.exports = {
